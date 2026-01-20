302e0d635a918c8dc4b2da32409f481b9fbe9389
Thiago4596 <116847445+Thiago4596@users.noreply.github.com>
2026-01-20 14:42:18 -0300

    Adiciona classe Jogo e atualiza template para exibir lista de jogos

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def olá():
-    return render_template('lista.html')
+    jogo1 = Jogo('Tétris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 
-app.run()
+
+    lista = [jogo1, jogo2, jogo3]
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+app.run(debug=True,)
